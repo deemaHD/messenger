b1f98db3096fcc36dea6c2e9612148f437fbc622
unknown <dima.selezen@rambler.ru>
2015-11-06 20:14:40 +0200

    Added validation

--- messenger/client/templates/authorization/login.js
+++ messenger/client/templates/authorization/login.js
@@ -10,31 +10,6 @@ Template.login.events({
         $('.login').addClass('hiden');
         $('.loginBtn').removeClass('active');
         $('.registrBtn').addClass('active');
-    },
-    'click #login-button': function (e) {
-        var email = $('#login-email').val(),
-            password = $('#login-password').val();
-
-        Meteor.loginWithPassword(email, password, function(err){
-            if (err) console.log(err);
-        });
-    },
-    'click #register-btn': function (e) {
-        var options = {
-            email: $('#email').val(),
-            username: $('#username').val(),
-            password: $('#password').val(),
-            location: $('#location').val()    
-        };
-
-        Accounts.createUser({ 
-            username: options.username, 
-            password: options.password, 
-            email: options.email, 
-            profile: {location: options.location}
-        }, function (err) {
-          if (err) console.log(err);
-        });
     }
 });
 
@@ -42,4 +17,61 @@ Template.login.helpers({
     locations: function () {
         return Locations.find();
     }
+});
+
+Template.login.onRendered(function () {
+    $('.loginForm').validate({
+        rules: {
+            email: {
+                required: true    
+            },
+            password: {
+                required: true,
+                minlength: 6
+            }
+        },
+        submitHandler: function (e) {            
+            var email = $('#login-email').val(),
+                password = $('#login-password').val();
+
+            Meteor.loginWithPassword(email, password, function (err) {
+                if (err) console.log(err);
+            });
+        }
+    });
+    $('.registrationForm').validate({
+        rules: {
+            email: {
+                required: true    
+            },
+            name: {
+                required: true,
+                minlength: 2
+            },
+            password: {
+                required: true,
+                minlength: 6
+            },
+            location: {
+                required: true
+            }
+        },
+        submitHandler: function (e) {
+            var options = {
+                email: $('#email').val(),
+                username: $('#username').val(),
+                password: $('#password').val(),
+                location: $('#location').val()    
+            };
+
+            Accounts.createUser({ 
+                username: options.username, 
+                password: options.password, 
+                email: options.email, 
+                profile: {location: options.location}
+            }, function (err) {
+              if (err) console.log(err);
+            });
+        }
+    });
 });
--- messenger/client/templates/settings/settings.js
+++ messenger/client/templates/settings/settings.js
@@ -1,41 +1,4 @@
-Template.settings.helpers({
-    locations: function() {
-        return Locations.find();
-    }
-});
-
-Template.settings.rendered = function () {
-    var user = Meteor.user();
-    
-    $('#inputEmail').val(user.emails[0].address);
-    $('#inputName').val(user.username);
-    $('#location').val(user.profile.location);
-};
-
 Template.settings.events({
-    'click .submitSettings': function () {
-        var options = {
-            email: $('#inputEmail').val(),
-            username: $('#inputName').val(),
-            location: $('#location').val()
-        };
-        
-        Meteor.users.update( Meteor.userId(), { $set: {
-            'emails.0.address': options.email,
-            'username': options.username,
-            'profile.location': options.location
-        }} );
-    },
-    'click .submitPassword': function () {
-        var options = {
-            oldPassword: $('#inputOldPassword').val(),
-            newPassword: $('#inputPassword').val()
-        };
-        
-        if (options.oldPassword && options.newPassword) {
-            Accounts.changePassword(options.oldPassword, options.newPassword);    
-        }
-    },
     'click .settingsBtn': function () {
         $('.userSettings').removeClass('hiden');
         $('.password').addClass('hiden');
@@ -49,3 +12,70 @@ Template.settings.events({
         $('.passwordBtn').addClass('active');
     }
 });
+
+Template.settings.helpers({
+    locations: function() {
+        return Locations.find();
+    }
+});
+
+Template.settings.rendered = function () {
+    var user = Meteor.user();
+    
+    $('#inputEmail').val(user.emails[0].address);
+    $('#inputName').val(user.username);
+    $('#location').val(user.profile.location);
+    
+    $('.settingsForm').validate({
+        rules: {
+            email: {
+                required: true
+            },
+            name: {
+                required: true,
+                minlength: 2
+            }
+        },
+        submitHandler: function (e) {
+            var options = {
+                email: $('#inputEmail').val(),
+                username: $('#inputName').val(),
+                location: $('#location').val()
+            };
+
+            Meteor.users.update( Meteor.userId(), { $set: {
+                'emails.0.address': options.email,
+                'username': options.username,
+                'profile.location': options.location
+            }} );
+            
+            $('.alertBox').html('<div class="alert alert-success">Settings changed</div>');
+        }
+    });
+    $('.passwordForm').validate({
+        rules: {
+            oldPassword: {
+                required: true,
+                minlength: 6
+            },
+            newPassword: {
+                required: true,
+                minlength: 6
+            }
+        },
+        submitHandler: function (e) {
+            var options = {
+                oldPassword: $('#inputOldPassword').val(),
+                newPassword: $('#inputPassword').val()
+            };
+
+            Accounts.changePassword(options.oldPassword, options.newPassword, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    $('.alertBox').html('<div class="alert alert-success">Password changed</div>');    
+                }
+            });
+        }
+    });
+};
